4917b251cf23946854f961d0610e57cb1a29f710
KaiserKatze <donizyo@gmail.com>
2018-03-18 23:10:31 +0800

    Update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+	strings: { [key: string]: string };
+
+	constructor() {
+		this.strings.join_now = '立即加入！';
+	}
 }
