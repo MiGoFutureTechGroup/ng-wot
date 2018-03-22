21b31ea2022092240ef15093596bc5563a4936ac
KaiserKatze <donizyo@gmail.com>
2018-03-22 21:01:25 +0800

    Add component
    @module 'session'
     - Add component 'join'
     - Add component 'login'

--- src/app/modules/session/session.module.ts
+++ src/app/modules/session/session.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LoginComponent } from './components/login/login.component';
+import { JoinComponent } from './components/join/join.component';
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: []
+	imports: [
+		CommonModule
+	],
+	declarations: [
+		LoginComponent,
+		JoinComponent
+	]
 })
 export class SessionModule { }
