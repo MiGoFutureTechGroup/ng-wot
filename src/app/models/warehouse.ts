b330455562ec597def887e2b6ec2d220dd5b7e93
KaiserKatze <donizyo@gmail.com>
2018-03-22 23:46:09 +0800

    Update model
    Update model 'warehouse'

--- src/app/models/warehouse.ts
+++ src/app/models/warehouse.ts
@@ -1,10 +1,29 @@
 export class WarehouseLog {
+
+	////////////////////////////
+	// 隐藏数据
+	////////////////////////////
+
+	// 到货日期
+	rcptDate: string = '';
 	// 登记日期
-	date: string;
+	rgstDate: string = '';
 	// 登记用户
-	userId: string;
+	userId: string = '';
+
+	////////////////////////////
+	// 可见数据
+	////////////////////////////
+
 	// 供应商
 	providerId: string;
 	// 仓库料号
 	partId: string;
+	// 数量
+	amount: number;
+	// 单位
+	unit: string;
+	// 备注
+	comment: string;
+
 }
