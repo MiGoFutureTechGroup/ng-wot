b49301b0e38413eddb957b3bb2a93ce5c4e1424c
KaiserKatze <donizyo@gmail.com>
2018-03-23 00:48:34 +0800

    Update component
    Update component 'warehouse'

--- src/app/components/warehouse/warehouse.component.ts
+++ src/app/components/warehouse/warehouse.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { OnInit, Component, ViewChild } from '@angular/core';
+import { MatTableDataSource, MatPaginator, MatSort } from '@angular/material';
+import { SelectionModel } from '@angular/cdk/collections';
+
+import { WarehouseLog } from '../../models/warehouse';
 
 @Component({
 	selector: 'app-warehouse',
@@ -7,9 +11,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WarehouseComponent implements OnInit {
 
+	units = [
+		{id: 1, name: 'pcs'},
+		{id: 2, name: 'kpcs'},
+	];
+	warehouseLogCache: WarehouseLog = {
+		rcptDate: '', rgstDate: '', userId: '', providerId: '', partId: '', amount: 0, unit: '', comment: '',
+	};
+	displayedColumns1 = ['rcptDate', 'providerId', 'partId', 'amount', 'unit', 'comment'];
+	displayedColumns2 = ['select', 'providerId', 'partId', 'amount', 'unit', 'comment'];
+	dataSource1 = new MatTableDataSource<WarehouseLog>([this.warehouseLogCache]);
+	dataSource2 = new MatTableDataSource<WarehouseLog>(MOCK_DATA);
+	selection = new SelectionModel<WarehouseLog>(true, []);
+	@ViewChild(MatSort) sort: MatSort;
+	@ViewChild(MatPaginator) paginator: MatPaginator;
+
 	constructor() { }
 
 	ngOnInit() {
 	}
 
 }
+
+const MOCK_DATA: WarehouseLog[] = [
+];
