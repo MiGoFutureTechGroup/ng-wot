91d2e3bf06717502802e95a9cb84230d66f9ded2
KaiserKatze <donizyo@gmail.com>
2018-03-23 20:26:24 +0800

    Update template
    Update template 'warehouse'
    Update stylesheet 'assets/table'

--- src/app/components/warehouse/warehouse.component.ts
+++ src/app/components/warehouse/warehouse.component.ts
@@ -5,31 +5,34 @@ import { SelectionModel } from '@angular/cdk/collections';
 import { WarehouseLog } from '../../models/warehouse';
 
 @Component({
-	selector: 'app-warehouse',
-	templateUrl: './warehouse.component.html',
-	styleUrls: ['./warehouse.component.scss']
+  selector: 'app-warehouse',
+  templateUrl: './warehouse.component.html',
+  styleUrls: ['./warehouse.component.scss']
 })
 export class WarehouseComponent implements OnInit {
 
-	units = [
-		{id: 1, name: 'pcs'},
-		{id: 2, name: 'kpcs'},
-	];
-	warehouseLogCache: WarehouseLog = {
-		rcptDate: '', rgstDate: '', userId: '', providerId: '', partId: '', amount: 0, unit: '', comment: '',
-	};
-	displayedColumns1 = ['rcptDate', 'providerId', 'partId', 'amount', 'unit', 'comment'];
-	displayedColumns2 = ['select', 'providerId', 'partId', 'amount', 'unit', 'comment'];
-	dataSource1 = new MatTableDataSource<WarehouseLog>([this.warehouseLogCache]);
-	dataSource2 = new MatTableDataSource<WarehouseLog>(MOCK_DATA);
-	selection = new SelectionModel<WarehouseLog>(true, []);
-	@ViewChild(MatSort) sort: MatSort;
-	@ViewChild(MatPaginator) paginator: MatPaginator;
+  units = [
+    {id: 1, name: 'pcs'},
+    {id: 2, name: 'kpcs'},
+  ];
+  warehouseLogCache: WarehouseLog = {
+    rcptDate: '', rgstDate: '', userId: '', providerId: '', partId: '', amount: undefined, unit: '', comment: '',
+  };
+  displayedColumns1 = ['rcptDate', 'providerId', 'partId', 'amount', 'unit', 'comment'];
+  displayedColumns2 = ['select', 'providerId', 'partId', 'amount', 'unit', 'comment'];
 
-	constructor() { }
+  dataSource = new MatTableDataSource<WarehouseLog>(MOCK_DATA);
+  selection = new SelectionModel<WarehouseLog>(true, []);
+  @ViewChild(MatSort) sort: MatSort;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
 
-	ngOnInit() {
-	}
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  applyFilter(value: string): void {
+  }
 
 }
 
