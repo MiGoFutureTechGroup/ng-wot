32543ed503979af294810db184663d6d8d60c2be
KaiserKatze <donizyo@gmail.com>
2018-03-29 23:57:25 +0800

    Update service 'MaterialDetailResolver'

--- src/app/components/materials/services/material-detail-resolver.service.ts
+++ src/app/components/materials/services/material-detail-resolver.service.ts
@@ -4,11 +4,28 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { Router, Resolve, RouterStateSnapshot, ActivatedRouteSnapshot } from '@angular/router';
 
-import { MaterialDetail } from './material-detail.component';
+import { Material } from '../../../models/material';
+import { MaterialService } from '../../../services/material.service';
 
 @Injectable()
-export class MaterialDetailResolverService implements Resolve<MaterialDetail> {
+export class MaterialDetailResolverService implements Resolve<Material> {
 
-  constructor() { }
+  constructor(
+      private service: MaterialService,
+      private router: Router
+  ) { }
+
+  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<Material> {
+    let id = route.paramMap.get('id');
+
+    return this.service.getMaterial(id).take(1).map(material => {
+      if (material) {
+        return material;
+      } else {
+        //this.router.navigate(['/404']);
+        return null;
+      }
+    });
+  }
 
 }
