3c496de1e0a17e7f7f60c7ac4eabf37badd3ab93
KaiserKatze <donizyo@gmail.com>
2018-03-31 22:31:57 +0800

    Refactor

--- src/app/models/user-type.ts
+++ src/app/models/user-type.ts
@@ -1,6 +1,6 @@
 import { Creation } from './creation';
 
 export class UserType extends Creation {
-	// 用户类型名称
-	name:	string;
+  // 用户类型名称
+  name: string;
 }
--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -2,14 +2,14 @@ import { UserType } from './user-type';
 import { Company } from './company';
 
 export class User {
-	// 用户 ID
-	id: 	string;
+  // 用户 ID
+  id: string;
   // 用户类型
   type: UserType;
-	// 用户名称
-	name:	string;
-	// 注册日期
-	date:	string;
+  // 用户名称
+  name: string;
+  // 注册日期
+  date: string;
   // 所属公司
   company: Company;
 
