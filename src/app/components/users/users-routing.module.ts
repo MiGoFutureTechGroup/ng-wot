ba589b5b33f11fda7d36db9c24c6e7cc85cbd7cf
KaiserKatze <donizyo@gmail.com>
2018-04-05 11:56:31 +0800

    Add resolver 'UserDetailResolver'

--- src/app/components/users/users-routing.module.ts
+++ src/app/components/users/users-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { UserDetailResolver } from './user-detail-resolver.service';
+
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [],
+  providers: [
+    UserDetailResolver,
+  ],
 })
 export class UsersRoutingModule { }
