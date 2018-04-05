c371ac7bab8660fc043c109c5e6da673b315bafa
KaiserKatze <donizyo@gmail.com>
2018-04-05 21:07:42 +0800

    Fix module 'User'

--- src/app/modules/user/user.module.ts
+++ src/app/modules/user/user.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 // router
 import { UserRoutingModule } from './user-routing.module';
@@ -16,6 +17,7 @@ import { UserService } from './services/user.service';
   imports: [
     CommonModule,
     UserRoutingModule,
+    FormsModule,
     MaterialModule,
   ],
   declarations: [
