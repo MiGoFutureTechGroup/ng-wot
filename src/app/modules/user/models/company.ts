0a4452f17a0f503643ebd814e4dad4d6ad09842f
KaiserKatze <donizyo@gmail.com>
2018-04-07 19:32:19 +0800

    Update model 'Company'

--- src/app/modules/user/models/company.ts
+++ src/app/modules/user/models/company.ts
@@ -5,8 +5,8 @@ export class Company {
     // 公司名称
     public name: string = null,
     // 公司地址
-    public location: string = null,
+    public location?: string,
     // 组织机构代码（organizing institution bar code）
-    public oibc: string = null,
+    public oibc?: string,
   ) { }
 }
