550315cbf441fc77b82047019ad4865f84caa93d
KaiserKatze <donizyo@gmail.com>
2018-04-07 22:49:58 +0800

    Update model 'Role'

--- src/app/modules/user/models/role.ts
+++ src/app/modules/user/models/role.ts
@@ -1,8 +1,22 @@
+import { User } from './user';
+import { Company } from './company';
 
 export class Role {
   constructor(
-    // 用户类型名称
+    // 用户分组名称
     public name: string = null,
+    // 用户父分组
+    public parent?: Role,
+
+    ////////////////////////////////////////////
+    // 元信息
+    ////////////////////////////////////////////
+    // 用户分组 ID
+    public id: string = null,
+    // 创建时间
+    public ctime?: Date,
+    // 创建者
+    public cid?: string,
   ) {
   }
 }
