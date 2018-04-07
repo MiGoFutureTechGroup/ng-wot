c35e4668a685a487f9c3df519cd2dff4db789572
KaiserKatze <donizyo@gmail.com>
2018-04-07 22:53:33 +0800

    Update component 'RoleComponent'

--- src/app/modules/user/components/role.component.ts
+++ src/app/modules/user/components/role.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit, Inject, Input } from '@angular/core';
-import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Location } from '@angular/common';
 
 import { Role } from '../models/role';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-user-type',
@@ -10,18 +12,28 @@ import { Role } from '../models/role';
 })
 export class RoleComponent implements OnInit {
 
-  @Input() type: Role;
+  activeRole: Role;
 
   constructor(
-    public dialogRef: MatDialogRef<RoleComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: any
+    private route: ActivatedRoute,
+    private router: Router,
+    private location: Location,
+    private userService: UserService,
   ) { }
 
   ngOnInit() {
+    this.route.data.subscribe((data: {activeRole: Role}) => {
+      this.activeRole = data.activeRole;
+    });
+  }
+
+  isViewNewRole(): boolean {
+    return this.location.isCurrentPathEqualTo('/users/new/role');
   }
 
   onCancel() {
-    this.dialogRef.close();
   }
+
+  onSubmit() {}
 
 }
