07fef827ceb9058c018a319d7c5de248ba62a10f
KaiserKatze <donizyo@gmail.com>
2018-05-13 12:34:56 +0800

    Update component 'LoginComponent'

--- src/app/modules/session/components/login/login.component.ts
+++ src/app/modules/session/components/login/login.component.ts
@@ -1,19 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
-	selector: 'app-login',
-	templateUrl: './login.component.html',
-	styleUrls: ['./login.component.scss']
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
 
-  code: string;
   username: string;
   password: string;
 
-	constructor() { }
+  constructor(
+    private http: HttpClient,
+  ) { }
 
-	ngOnInit() {
-	}
+  ngOnInit() {
+  }
+
+  login(): void {
+    let url: string = '/api/login/';
+    let body: any = {
+      'data': {
+        'username': this.username,
+        'password': this.password
+      }
+    };
+    console.log('Login attempt:', url, body);
+
+    body = JSON.stringify(body);
+    this.http.post<any>(url, body).subscribe((res) => {
+      console.log('Response:', res);
+    });
+    // TODO handle ajax response
+  }
 
 }
