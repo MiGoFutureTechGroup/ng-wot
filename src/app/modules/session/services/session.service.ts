75e7d229f5c4a8398699cf8c44ead2c7a3d52cec
KaiserKatze <donizyo@gmail.com>
2018-05-14 23:50:29 +0800

    Update service 'SessionService'

--- src/app/modules/session/services/session.service.ts
+++ src/app/modules/session/services/session.service.ts
@@ -3,6 +3,16 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class SessionService {
 
+  private loginState: boolean = false;
+
 	constructor() { }
+
+  set isLoggedIn(loginState: boolean) {
+    this.loginState = loginState;
+  }
+
+  get isLoggedIn() {
+    return this.loginState;
+  }
 
 }
